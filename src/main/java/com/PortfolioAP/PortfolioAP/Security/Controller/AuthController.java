4b8d798e66b4bfee616d6de0e13e5ab588446a01
Facundo Comercio <facundocomercio@outlook.com>
2023-03-17 02:00:36 -0300

    Probando con un solo link de firebase

--- src/main/java/com/PortfolioAP/PortfolioAP/Security/Controller/AuthController.java
+++ src/main/java/com/PortfolioAP/PortfolioAP/Security/Controller/AuthController.java
@@ -39,7 +39,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"https://portfolioap-ed82a.web.app/","http://localhost:4200"})
+@CrossOrigin(origins = "https://portfolioap-ed82a.web.app")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
